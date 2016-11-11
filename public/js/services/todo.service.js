ff0161f73cf8ef0eba3581f24464d4d9a669cc5a
AnuThogara <anuradha.thogara@gmail.com>
2016-11-10 20:49:00 -0500

    established get all service methods that connects fron end and backend

--- public/js/services/todo.service.js
+++ public/js/services/todo.service.js
@@ -3,11 +3,41 @@
   angular.module('mean-todos')
         .factory('TodoService', TodoService);
 
-  TodoService.$inject = [];
+  TodoService.$inject = ['$http'];
 
-  function TodoService(){
+  function TodoService($http){
+    init();
+    var todos = [];
     return {
-
+     get: getAllTodos,
+     create: createOneTodo,
+     update: updateOneTodo,
+     delete: deleteOneTodo
     };
+     function init() { //provide to initializes
+
+       $http.get('./todos')
+            .then(function(response){
+              todos = response.data.todos;
+            })
+            .catch(function(err){
+              console.log(err);
+            });
+      }
+    function getAllTodos(){
+      return todos;
+    }
+    function createOneTodo(todo){
+      DEBUGGER();
+      $http.post('./todos', todo)
+         .then(function(response){
+           todos.push(todo);
+         })
+         .catch(function(err){
+           console.log(err);
+         });
+    }
+    function updateOneTodo(index, todo){}
+    function deleteOneTodo(index){}
   }
 }());
